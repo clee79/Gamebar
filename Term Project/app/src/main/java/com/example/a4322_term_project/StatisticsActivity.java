d0552e70f5a456d0b19dd42f98788a4cd034390f
Matthew Bowers <matbowrs@gmail.com>
2020-04-26 14:15:55 -0400

    Took out unecessary code, small styling. Committing before I try to fix stats page

--- Term Project/app/src/main/java/com/example/a4322_term_project/StatisticsActivity.java
+++ Term Project/app/src/main/java/com/example/a4322_term_project/StatisticsActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -35,7 +39,6 @@ public class StatisticsActivity extends AppCompatActivity {
 
     ListView listView;
     ArrayList<String> array = new ArrayList<>();
-    ArrayList<Stat> statArray = new ArrayList<>();
     TextView statsTV;
     ImageView back;
     int totalGames;
--- Term Project/app/src/main/java/com/example/a4322_term_project/Summary.java
+++ Term Project/app/src/main/java/com/example/a4322_term_project/Summary.java
@@ -11,19 +11,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Summary extends AppCompatActivity {
 
@@ -36,7 +32,6 @@ public class Summary extends AppCompatActivity {
     // Establish connection to firebase
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
-    FirebaseFirestore fStore;
     String userID;
 
 
@@ -126,8 +121,6 @@ public class Summary extends AppCompatActivity {
 
     public void storeGameScore (int topic, int right) {
         // Get random key for document
-        String key = getKey();
-
         String dbTopic = getTopicName(topic);
         String dbCorrect = Integer.toString(right);
         String dbQuizDate = getDate();
@@ -154,25 +147,6 @@ public class Summary extends AppCompatActivity {
                 }
             }
         });
-
-
-        Log.d("TAG", "GENERATED KEY " + key);
-
-        // insert data to map
-        /*
-        Map <String, Object> quiz = new HashMap<>();
-        quiz.put("userID", userID);
-        quiz.put("topic", dbTopic);
-        quiz.put("correct", dbCorrect);
-        quiz.put("data", dbQuizDate);
-
-
-        documentReference.set(quiz).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Log.d("DB", "onSuccess: user profile is created -> " + userID);
-            }
-        });*/
     }
 
     public String getDate() {
@@ -181,26 +155,5 @@ public class Summary extends AppCompatActivity {
         String strDate = dateFormat.format(date).toString();
 
         return strDate;
-    }
-
-    public String getKey () {
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                + "0123456789"
-                + "abcdefghijklmnopqrstuvxyz";
-
-        // create StringBuffer size of AlphaNumericString
-        StringBuilder sb = new StringBuilder(16);
-
-        for (int i = 0; i < 16; i++) {
-
-            // generate a random number between
-            // 0 to AlphaNumericString variable length
-            int index = (int)(AlphaNumericString.length() * Math.random());
-
-            // add Character one by one in end of sb
-            sb.append(AlphaNumericString.charAt(index));
-        }
-
-        return sb.toString();
     }
 }
